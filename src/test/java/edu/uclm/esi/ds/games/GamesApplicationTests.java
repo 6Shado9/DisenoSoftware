f6ffa9d4c26b8f8eeef1349d351ff5aa31bd1b8e
6Shado9 <73654887+6Shado9@users.noreply.github.com>
2023-02-04 18:44:21 +0100

    Creacion del proyecto con spring

---
+++ src/test/java/edu/uclm/esi/ds/games/GamesApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.uclm.esi.ds.games;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
